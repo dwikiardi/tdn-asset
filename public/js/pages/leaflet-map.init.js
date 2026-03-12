85ab1a783c004cd65bbc2374cde02ded6f7d30b8
unknown <dwiardi60@gmail.com>
2026-03-12 13:41:20 +0800

    feat: update map data and clean up for github upload

--- public/js/pages/leaflet-map.init.js
+++ public/js/pages/leaflet-map.init.js
@@ -139,7 +139,7 @@ var geojson = L.geoJson(statesData, { style: style }).addTo(interactivemap),
 var mbAttr =
     'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     mbUrl =
-        "",
+        "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw",
     grayscale = L.tileLayer(mbUrl, {
         id: "mapbox/light-v9",
         tileSize: 512,
